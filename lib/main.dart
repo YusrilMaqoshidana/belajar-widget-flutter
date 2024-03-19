3153a1d9e6a27485294c7ade578e7e0eeba3df20
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-03-20 01:26:14 +0700

    Membuat beberapa card menu

---
+++ lib/main.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Menu());
+}
+
+class Menu extends StatelessWidget {
+  const Menu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue[50],
+        appBar: AppBar(
+          title: const Text(
+            "Daftar Menu",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    "Menu makanan",
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  )),
+              Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 200,
+                      margin: const EdgeInsets.only(right: 20.0),
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: const Image(
+                          image:
+                              AssetImage('../Assets/image/kentanggoreng.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Kentang Goreng",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        const Text(
+                          "Harga : Rp. 8.000",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: const Column(
+                            // mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Tambahkan fungsi untuk tombol di sini
+                            Color(Colors.lightBlue as int);
+                          },
+                          child: const Text(
+                            "Pesan",
+                            style:
+                                TextStyle(fontFamily: 'Poppins', fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+              Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 200,
+                      margin: const EdgeInsets.only(right: 20.0),
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: const Image(
+                          image: AssetImage('../Assets/image/nasigoreng.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Kentang Goreng",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        const Text(
+                          "Harga : Rp. 12.000",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: const Column(
+                            // mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Tambahkan fungsi untuk tombol di sini
+                          },
+                          child: const Text(
+                            "Pesan",
+                            style:
+                                TextStyle(fontFamily: 'Poppins', fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+              Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 200,
+                      margin: const EdgeInsets.only(right: 20.0),
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: const Image(
+                          image: AssetImage('../Assets/image/mierebus.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Mie Rebus",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        const Text(
+                          "Harga : Rp. 7.000",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: const Column(
+                            // mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Tambahkan fungsi untuk tombol di sini
+                          },
+                          child: const Text(
+                            "Pesan",
+                            style:
+                                TextStyle(fontFamily: 'Poppins', fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
